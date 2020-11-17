020aeb83f1451ef8ff673afbac2e61fb0efa3a20
1nkribbon <seregio@rambler.ru>
2020-11-17 19:49:13 +0300

    different languages

--- test_items.py
+++ test_items.py
@@ -5,8 +5,8 @@ link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 
 def test_add_to_cart_button_existance(browser):
-	browser.get(link)
-	time.sleep(30)
-	add_to_cart_button = []
-	add_to_cart_button = browser.find_elements_by_css_selector(".btn-add-to-basket")
-	assert len(add_to_cart_button) == 1
+    browser.get(link)
+    time.sleep(30)
+    add_to_cart_button = []
+    add_to_cart_button = browser.find_elements_by_css_selector(".btn-add-to-basket")
+    assert len(add_to_cart_button) == 1
